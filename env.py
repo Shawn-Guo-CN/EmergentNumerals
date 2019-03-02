0c786d1eff59c08d0ffc4cda3c761f7a7b97caa5
Shawn Guo <shawnguo.cn@gmail.com>
2019-03-02 13:38:42 +0000

    use a "stupid" way to move the whole environment to GPU

--- env.py
+++ env.py
@@ -53,41 +53,42 @@ class FoodGatherEnv(object):
 
 
 class FoodGatherEnv_GPU(object):
-    def __init__(self, num_food_types=3, max_capacity=5):
+    def __init__(self, num_food_types=3, max_capacity=5,  device=torch.device("cpu")):
         """
         This is the game environment.
         :param max_capacity: int, the maximum capacity for specific kind of food
         """
         self.max_capacity = max_capacity
         self.num_food_types = num_food_types
+        self.device = device
 
-        self.warehouse_num = torch.randint(0, self.max_capacity + 1, (self.num_food_types,), dtype=torch.int8)
-        self.knapsack_num = torch.zeros((self.num_food_types,), dtype=torch.int8)
-        self.expected_num = self.max_capacity * torch.ones((self.num_food_types,), dtype=torch.int8)
+        self.warehouse_num = torch.randint(0, self.max_capacity + 1, (self.num_food_types,), dtype=torch.int8).to(device)
+        self.knapsack_num = torch.zeros((self.num_food_types,), dtype=torch.int8).to(device)
+        self.expected_num = self.max_capacity * torch.ones((self.num_food_types,), dtype=torch.int8).to(device)
 
         self.num_actions = num_food_types + 1
-        action2shift_eye = torch.eye(self.num_food_types, dtype=torch.int8)
-        action2shift_end = torch.zeros((1, self.num_food_types), dtype=torch.int8)
-        self.action2shift = torch.cat((action2shift_eye, action2shift_end), dim=0)
+        action2shift_eye = torch.eye(self.num_food_types, dtype=torch.int8).to(device)
+        action2shift_end = torch.zeros((1, self.num_food_types), dtype=torch.int8).to(device)
+        self.action2shift = torch.cat((action2shift_eye, action2shift_end), dim=0).to(device)
 
         self.knapsack_max = 10
 
     def step(self, action):
         self.knapsack_num += self.action2shift[action]
-        self.knapsack_num = torch.clamp(self.knapsack_num, max=self.knapsack_max)
-        reward = torch.zeros((1,), dtype=torch.float)
+        self.knapsack_num = torch.clamp(self.knapsack_num, max=self.knapsack_max).to(self.device)
+        reward = torch.zeros((1,), dtype=torch.float).to(self.device)
         if action == self.num_food_types:
             if torch.equal(self.expected_num, self.knapsack_num + self.warehouse_num):
-                reward = 100 * torch.ones((1,), dtype=torch.float)
+                reward = 100 * torch.ones((1,), dtype=torch.float).to(self.device)
             else:
-                reward = -100 * torch.ones((1,), dtype=torch.float)
+                reward = -100 * torch.ones((1,), dtype=torch.float).to(self.device)
             return self.knapsack_num, reward, True
         else:
             return self.knapsack_num, reward, False
 
     def reset(self):
-        self.warehouse_num = torch.randint(0, self.max_capacity + 1, (self.num_food_types,))
-        self.knapsack_num = torch.zeros((self.num_food_types,), dtype=torch.int8)
+        self.warehouse_num = torch.randint(0, self.max_capacity + 1, (self.num_food_types,)).to(self.device)
+        self.knapsack_num = torch.zeros((self.num_food_types,), dtype=torch.int8).to(self.device)
         return self.knapsack_num
 
 
@@ -104,8 +105,9 @@ def test_food_gather_env_by_hand(env):
 if __name__ == '__main__':
     cf = configparser.ConfigParser()
     cf.read('./game.conf')
-    env = FoodGatherEnv(int(cf.defaults()['num_food_types']),
-                        int(cf.defaults()['max_capacity']))
-    # env = FoodGatherEnv_GPU(int(cf.defaults()['num_food_types']),
-    #                         int(cf.defaults()['max_capacity']))
+    # env = FoodGatherEnv(int(cf.defaults()['num_food_types']),
+    #                     int(cf.defaults()['max_capacity']))
+    env = FoodGatherEnv_GPU(int(cf.defaults()['num_food_types']),
+                            int(cf.defaults()['max_capacity']),
+                            torch.device("cuda"))
     test_food_gather_env_by_hand(env)
