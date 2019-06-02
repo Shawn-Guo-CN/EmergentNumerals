3d5d742c1b3778ab17dd7630483a53d0b9cb9da9
shawn <shawnguo.cn@gmail.com>
2019-06-02 16:12:30 +0100

    set a seperate optimiser to decoder

--- train_models/train_seq2seq.py
+++ train_models/train_seq2seq.py
@@ -4,9 +4,10 @@ from preprocesses.DataIterator import FruitSeqDataset
 from preprocesses.Voc import Voc
 
 
-def train_epoch(data_batch, model, param_optimizer, clip=CLIP):
+def train_epoch(data_batch, model, param_optimiser, decoder_optimiser, clip=CLIP):
     # Zero gradients
-    param_optimizer.zero_grad()
+    param_optimiser.zero_grad()
+    decoder_optimiser.zero_grad()
 
     # Forward pass through model
     loss, print_losses, n_totals = model(data_batch)
@@ -18,7 +19,8 @@ def train_epoch(data_batch, model, param_optimizer, clip=CLIP):
     nn.utils.clip_grad_norm_(model.parameters(), clip)
 
     # Adjust model weights
-    param_optimizer.step()
+    param_optimiser.step()
+    decoder_optimiser.step()
 
     return sum(print_losses) / n_totals
 
@@ -36,14 +38,14 @@ def train():
     print('building model...')
     seq2seq = Seq2Seq(voc.num_words)
     param_optimizer = OPTIMISER(seq2seq.parameters(), lr=LEARNING_RATE)
-    #TODO: find out how to update this part
-    # decoder_optimizer = OPTIMISER(decoder.parameters(), lr=LEARNING_RATE * DECODER_LEARING_RATIO)
+    decoder_optimizer = OPTIMISER(seq2seq.decoder.parameters(), \
+                                    lr=LEARNING_RATE * DECODER_LEARING_RATIO)
     if PARAM_FILE is not None:
         print('\tloading saved parameters from ' + PARAM_FILE + '...')
         checkpoint = torch.load(PARAM_FILE)
         seq2seq.load_state_dict(checkpoint['model'])
         param_optimizer.load_state_dict(checkpoint['opt'])
-        # decoder_optimizer.load_state_dict(checkpoint['de_opt'])
+        decoder_optimizer.load_state_dict(checkpoint['ddecoder_optimizere_opt'])
         voc = checkpoint['voc']
         print('\tdone')
     print('done')
@@ -56,7 +58,7 @@ def train():
     print('training...')
     for iter in range(start_iteration, NUM_ITERS+1):
         for idx, data_batch in enumerate(train_set):
-            loss = train_epoch(data_batch, seq2seq, param_optimizer)
+            loss = train_epoch(data_batch, seq2seq, param_optimizer, decoder_optimizer)
             print_loss += loss
 
         if iter % PRINT_EVERY == 0:
@@ -73,7 +75,7 @@ def train():
                 'iteration': iter,
                 'model': seq2seq.state_dict(),
                 'opt': param_optimizer.state_dict(),
-                # 'de_opt': decoder_optimizer.state_dict(),
+                'de_opt': decoder_optimizer.state_dict(),
                 'loss': loss,
                 'voc': voc
             }, os.path.join(directory, '{}_{}.tar'.format(iter, 'checkpoint')))
