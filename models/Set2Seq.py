94758ad1a9e958b546d98320d1aaf901acb5a074
shawn <shawnguo.cn@gmail.com>
2019-06-02 17:09:43 +0100

    updated and reconstructed Set2Seq model

--- models/Set2Seq.py
+++ models/Set2Seq.py
@@ -28,13 +28,12 @@ class Attn(nn.Module):
         return attn_weights.transpose(0, 1).transpose(1, 2)
 
 class EncoderLSTM(nn.Module):
-    def __init__(self, embedding, hidden_size=HIDDEN_SIZE, max_length=MAX_LENGTH+2):
+    def __init__(self, voc_size, hidden_size=HIDDEN_SIZE, max_length=MAX_LENGTH+2):
         super(EncoderLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.max_length = max_length
 
-        self.embedding = embedding
-        self.memorising = nn.Linear(self.hidden_size, self.hidden_size)
+        self.memorising = nn.Embedding(voc_size, self.hidden_size)
         self.attn = Attn(hidden_size)
         self.dropout = nn.Dropout(DROPOUT_RATIO)
         self.lstm = nn.LSTM(hidden_size, hidden_size)
@@ -42,14 +41,11 @@ class EncoderLSTM(nn.Module):
         self.init_hidden = self.init_hidden_and_cell()
         self.init_cell = self.init_hidden_and_cell()
 
-    def forward(self, whole_input, current_input, last_hidden, last_cell):
-        # Embed the current input element
-        embedded = self.dropout(self.embedding(current_input))
-
+    def forward(self, whole_input, cur_input_embbeded, last_hidden, last_cell):
         # Calculate the memory vectors for the whole input sequence
-        memorised = self.dropout(self.memorising(self.embedding(whole_input)))
+        memorised = self.memorising(whole_input)
         # Calculate attention weights from the current LSTM input
-        attn_weights = self.attn(embedded, memorised)
+        attn_weights = self.attn(cur_input_embbeded, memorised)
         # Multiply attention weights to the memory vector to get new "weighted sum" memory vector
         r = attn_weights.bmm(memorised.transpose(0, 1)).transpose(0, 1)
         # Forward through unidirectional LSTM
@@ -64,24 +60,19 @@ class EncoderLSTM(nn.Module):
 
 
 class DecoderLSTM(nn.Module):
-    def __init__(self, output_size, embedding, hidden_size=HIDDEN_SIZE, dropout=DROPOUT_RATIO):
+    def __init__(self, output_size, hidden_size=HIDDEN_SIZE, dropout=DROPOUT_RATIO):
         super(DecoderLSTM, self).__init__()
         self.hidden_size = hidden_size
 
-        self.embedding = embedding
         self.lstm = nn.LSTM(hidden_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, last_input, last_hidden, last_cell):
-        # embedded size = [1, batch size, emb dim]
-        embedded = self.dropout(self.embedding(last_input))
-        
         # output = [sent len, batch size, hid dim * n directions]
         # hidden = [1, batch size, hid dim]
         # cell = [1, batch size, hid dim]
-        output, (hidden, cell) = self.lstm(embedded, (last_hidden, last_cell))
+        output, (hidden, cell) = self.lstm(last_input, (last_hidden, last_cell))
         # sent len and n directions will always be 1 in the decoder, therefore:
         # output = [1, batch size, hid dim]
         # hidden = [batch size, hid dim]
@@ -95,3 +86,79 @@ class DecoderLSTM(nn.Module):
     def init_hidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=DEVICE)
 
+
+class Set2Seq(nn.Module):
+    def __init__(self, voc_size, hidden_size=HIDDEN_SIZE, dropout=DROPOUT_RATIO):
+        super(Set2Seq, self).__init__()
+        self.voc_size = voc_size
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(self.voc_size, self.hidden_size)
+        self.dropout = nn.Dropout(dropout)
+
+        self.encoder = EncoderLSTM(self.voc_size, self.hidden_size)
+        self.decoder = DecoderLSTM(self.voc_size, self.hidden_size)
+
+    def forward(self, data_batch):
+        input_var = data_batch['input']
+        input_mask = data_batch['input_mask']
+        target_var = data_batch['target']
+        target_mask = data_batch['target_mask']
+        target_max_len = data_batch['target_max_len']
+
+        batch_size = input_var.shape[1]
+        batch_length = input_var.shape[0]
+
+        # Initialize variables
+        loss = 0
+        print_losses = []
+        n_totals = 0
+
+        # TODO: replace repeat with view
+        # initialise the initial hidden and cell states for encoder
+        encoder_hidden = self.encoder.init_hidden.repeat(1, batch_size, 1)
+        encoder_cell = self.encoder.init_cell.repeat(1, batch_size, 1)
+
+        # Forward pass through encoder
+        for t in range(batch_length):
+            encoder_input = self.embedding(input_var[t])
+            cur_hidden, cur_cell = \
+                self.encoder(input_var, encoder_input, encoder_hidden, encoder_cell)
+            # TODO: replace reshape with view
+            cur_weights = input_mask[t].reshape(1, -1, 1).to(encoder_hidden.dtype)
+            last_weights = (1 - input_mask[t]).reshape(1, -1, 1).to(encoder_hidden.dtype)
+            encoder_hidden = last_weights * encoder_hidden + cur_weights * cur_hidden
+            encoder_cell = last_weights * encoder_cell + cur_weights * cur_cell
+
+        # Create initial decoder input (start with SOS tokens for each sentence)
+        decoder_input = self.embedding(
+            torch.LongTensor([[SOS_INDEX for _ in range(batch_size)]]).to(DEVICE)
+        )
+
+        # Set initial decoder hidden state to the encoder's final hidden state
+        decoder_hidden = encoder_hidden
+        decoder_cell = encoder_cell
+
+        # Determine if we are using teacher forcing this iteration
+        use_teacher_forcing = True if random.random() < TEACHER_FORCING_RATIO \
+                                    and self.training else False
+
+        # Forward batch of sequences through decoder one time step at a time
+        for t in range(target_max_len):
+            decoder_output, decoder_hidden, decoder_cell = \
+                self.decoder(decoder_input, decoder_hidden, decoder_cell)
+
+            if use_teacher_forcing:
+                decoder_input = self.embedding(target_var[t].view(1, -1))
+            else:
+                _, topi = decoder_output.topk(1)
+                decoder_input = self.embedding(
+                    torch.LongTensor([[topi[i][0] for i in range(batch_size)]]).to(DEVICE)
+                )
+                
+            mask_loss, n_total = mask_NLL_loss(decoder_output, target_var[t], target_mask[t])
+            loss += mask_loss
+            print_losses.append(mask_loss.item() * n_total)
+            n_totals += n_total
+
+        return loss, print_losses, n_totals
--- train_models/train_set2seq.py
+++ train_models/train_set2seq.py
@@ -1,188 +1,66 @@
 from utils.conf import *
 from models.Set2Seq import *
-from utils.DatasetLoader import *
-from utils.Preprocesses import *
+from preprocesses.DataIterator import FruitSeqDataset
+from preprocesses.Voc import Voc
 
 
-def encoder_forward(encoder, input_variable, input_mask):
-    batch_size = input_variable.shape[1]
-    batch_length = input_variable.shape[0]
-
-    # initialise the initial hidden and cell states for encoder
-    encoder_hidden = encoder.init_hidden.repeat(1, batch_size, 1)
-    encoder_cell = encoder.init_cell.repeat(1, batch_size, 1)
-
-    # Forward pass through encoder
-    for t in range(batch_length):
-        encoder_input = input_variable[t]
-        cur_hidden, cur_cell = \
-            encoder(input_variable, encoder_input, encoder_hidden, encoder_cell)
-        cur_weights = input_mask[t].reshape(1, -1, 1).to(encoder_hidden.dtype)
-        last_weights = (1 - input_mask[t]).reshape(1, -1, 1).to(encoder_hidden.dtype)
-        encoder_hidden = last_weights * encoder_hidden + cur_weights * cur_hidden
-        encoder_cell = last_weights * encoder_cell + cur_weights * cur_cell
-
-    return encoder_hidden, encoder_cell
-
-
-def train_epoch(input_variable, input_mask, target_variable, target_mask, target_max_len, 
-                    encoder, decoder, encoder_optimizer, decoder_optimizer, clip=CLIP):
-    batch_size = input_variable.shape[1]
-    batch_length = input_variable.shape[0]
-    
+def train_epoch(model, data_batch, param_optimizer, decoder_optimizer, clip=CLIP):
     # Zero gradients
-    encoder_optimizer.zero_grad()
+    param_optimizer.zero_grad()
     decoder_optimizer.zero_grad()
 
-    # Initialize variables
-    loss = 0
-    print_losses = []
-    n_totals = 0
-
-    encoder_hidden, encoder_cell = encoder_forward(encoder, input_variable, input_mask)
-
-    # Create initial decoder input (start with SOS tokens for each sentence)
-    decoder_input = torch.LongTensor([[SOS_INDEX for _ in range(batch_size)]]).to(DEVICE)
-
-    # Set initial decoder hidden state to the encoder's final hidden state
-    decoder_hidden = encoder_hidden
-    decoder_cell = encoder_cell
-
-    # Determine if we are using teacher forcing this iteration
-    use_teacher_forcing = True if random.random() < TEACHER_FORCING_RATIO else False
-
-    # Forward batch of sequences through decoder one time step at a time
-    for t in range(target_max_len):
-        decoder_output, decoder_hidden, decoder_cell = \
-            decoder(decoder_input, decoder_hidden, decoder_cell)
-
-        if use_teacher_forcing:
-            decoder_input = target_variable[t].view(1, -1)
-        else:
-            _, topi = decoder_output.topk(1)
-            decoder_input = torch.LongTensor([[topi[i][0] for i in range(batch_size)]]).to(DEVICE)
-        
-        mask_loss, n_total = mask_NLL_loss(decoder_output, target_variable[t], target_mask[t])
-        loss += mask_loss
-        print_losses.append(mask_loss.item() * n_total)
-        n_totals += n_total
-
+    # Forward pass through model
+    loss, print_losses, n_totals = model(data_batch)
     # Perform backpropatation
     loss.backward()
 
     # Clip gradients: gradients are modified in place
-    nn.utils.clip_grad_norm_(encoder.parameters(), clip)
-    nn.utils.clip_grad_norm_(decoder.parameters(), clip)
+    nn.utils.clip_grad_norm_(model.parameters(), clip)
 
     # Adjust model weights
-    encoder_optimizer.step()
+    param_optimizer.step()
     decoder_optimizer.step()
 
     return sum(print_losses) / n_totals
 
 
-def dev_epoch(input_variable, input_mask, target_variable, target_mask, target_max_len, \
-                encoder, decoder, clip=CLIP):
-    batch_size = input_variable.shape[1]
-    batch_length = input_variable.shape[0]
-
-    # Initialize variables
-    loss = 0
-    print_losses = []
-    n_totals = 0
-    
-    # Forward pass through encoder
-    encoder_hidden, encoder_cell = encoder_forward(encoder, input_variable, input_mask)
-
-    # Create initial decoder input (start with SOS tokens for each sentence)
-    decoder_input = torch.LongTensor([[SOS_INDEX for _ in range(batch_size)]]).to(DEVICE)
-
-    # Set initial decoder hidden state to the encoder's final hidden state
-    decoder_hidden = encoder_hidden
-    decoder_cell = encoder_cell
-
-    for t in range(target_max_len):
-        decoder_output, decoder_hidden, decoder_cell = \
-            decoder(decoder_input, decoder_hidden, decoder_cell)
-        
-        # No teacher forcing: next input is decoder's own current output
-        _, topi = decoder_output.topk(1)
-        decoder_input = torch.LongTensor([[topi[i][0] for i in range(batch_size)]]).to(DEVICE)
-
-        # Calculate and accumulate loss
-        mask_loss, n_total = mask_NLL_loss(decoder_output, target_variable[t], target_mask[t])
-        loss += mask_loss
-        print_losses.append(mask_loss.item() * n_total)
-        n_totals += n_total
-
-    return sum(print_losses) / n_totals
-
-
 def train():
-    print('loading datasets...')
-    train_set, dev_set, test_set = load_train_dev_test()
-    print('\t', len(train_set), len(dev_set), len(test_set), 'items in total.')
-    print('done')
-
+    print('building vocabulary...')
     voc = Voc()
-
-    print('converting datasets...')
-    train_set = string_set2indices_pair_set(voc, train_set)
-    dev_set = string_set2indices_pair_set(voc, dev_set)
-    # test_set = string_set2indices_set(voc, test_set)
     print('done')
 
-    print('generating batches...')
-    train_input_batches, train_input_mask_batches, train_input_lens_batches, \
-        train_target_batches, train_target_mask_batches, train_target_max_len_batches \
-            = indices_pair_set2data_batches(train_set)
-    dev_input_batches, dev_input_mask_batches, dev_input_lens_batches, \
-        dev_target_batches, dev_target_mask_batches, dev_target_max_len_batches \
-            = indices_pair_set2data_batches(dev_set)
-    # test_input_batches, test_target_batches = indices_set2data_batches(test_set)
+    print('loading data and building batches...')
+    train_set = FruitSeqDataset(voc, dataset_file_path=TRAIN_FILE_PATH)
+    dev_set = FruitSeqDataset(voc, dataset_file_path=DEV_FILE_PATH)
+    # test_set = FruitSeqDataset(voc, dataset_file_path=TEST_FILE_PATH)
     print('done')
     
     print('building model...')
-    embedding = nn.Embedding(voc.num_words, HIDDEN_SIZE).to(DEVICE)
-    encoder = EncoderLSTM(embedding).to(DEVICE)
-    decoder = DecoderLSTM(voc.num_words, embedding).to(DEVICE)
-    encoder_optimizer = OPTIMISER(encoder.parameters(), lr=LEARNING_RATE)
-    decoder_optimizer = OPTIMISER(decoder.parameters(), lr=LEARNING_RATE * DECODER_LEARING_RATIO)
+    set2seq = Set2Seq(voc.num_words).to(DEVICE)
+    param_optimizer = OPTIMISER(set2seq.parameters(), lr=LEARNING_RATE)
+    decoder_optimizer = OPTIMISER(set2seq.decoder.parameters(), 
+                                    lr=LEARNING_RATE * DECODER_LEARING_RATIO)
     if PARAM_FILE is not None:
         print('\tloading saved parameters from ' + PARAM_FILE + '...')
         checkpoint = torch.load(PARAM_FILE)
-        encoder.load_state_dict(checkpoint['encoder'])
-        decoder.load_state_dict(checkpoint['decoder'])
-        encoder_optimizer.load_state_dict(checkpoint['en_opt'])
+        set2seq.load_state_dict(checkpoint['model'])
+        param_optimizer.load_state_dict(checkpoint['opt'])
         decoder_optimizer.load_state_dict(checkpoint['de_opt'])
-        embedding.load_state_dict(checkpoint['embedding'])
         voc = checkpoint['voc']
         print('\tdone')
     print('done')
     
-    print('initiprint_lossesalising...')
+    print('initialising...')
     start_iteration = 1
     print_loss = 0
     print('done')
 
     print('training...')
     for iter in range(start_iteration, NUM_ITERS+1):
-        for batch_index in range(0, len(train_input_batches)):
-            input_batch = train_input_batches[batch_index]
-            input_mask_batch = train_input_mask_batches[batch_index]
-            target_batch = train_target_batches[batch_index]
-            target_mask_batch = train_target_mask_batches[batch_index]
-            target_max_len_batch = train_target_max_len_batches[batch_index]
-
-            loss = train_epoch(
-                input_batch,
-                input_mask_batch,
-                target_batch,
-                target_mask_batch,
-                target_max_len_batch,
-                encoder,
-                decoder,
-                encoder_optimizer, 
+        for idx, data_batch in enumerate(train_set):
+            loss = train_epoch(set2seq,
+                data_batch,
+                param_optimizer,
                 decoder_optimizer
             )
             print_loss += loss
@@ -194,44 +72,29 @@ def train():
             print_loss = 0
 
         if iter % SAVE_EVERY == 0:
-            directory = os.path.join(SAVE_DIR, 'standard_seq2seq_' + str(HIDDEN_SIZE))
+            directory = os.path.join(SAVE_DIR, 'set2seq_' + str(HIDDEN_SIZE))
             if not os.path.exists(directory):
                 os.makedirs(directory)
             torch.save({
                 'iteration': iter,
-                'encoder': encoder.state_dict(),
-                'decoder': decoder.state_dict(),
-                'en_opt': encoder_optimizer.state_dict(),
+                'model': set2seq.state_dict(),
+                'opt': param_optimizer.state_dict(),
                 'de_opt': decoder_optimizer.state_dict(),
                 'loss': loss,
-                'voc': voc,
-                'embedding': embedding.state_dict()
+                'voc': voc
             }, os.path.join(directory, '{}_{}.tar'.format(iter, 'checkpoint')))
 
         if iter % EVAL_EVERY == 0:
-            encoder.eval()
-            decoder.eval()
+            set2seq.eval()
+            
             loss = 0
-            for batch_index in range(0, len(dev_input_batches)):
-                input_batch = dev_input_batches[batch_index]
-                input_mask_batch = dev_input_mask_batches[batch_index]
-                target_batch = dev_target_batches[batch_index]
-                target_mask_batch = dev_target_mask_batches[batch_index]
-                target_max_len_batch = dev_target_max_len_batches[batch_index]
-                
-                loss += dev_epoch(
-                    input_batch,
-                    input_mask_batch,
-                    target_batch,
-                    target_mask_batch,
-                    target_max_len_batch,
-                    encoder,
-                    decoder
-                )
+            for idx, data_batch in enumerate(dev_set):
+                _, print_losses, n_totals = set2seq(data_batch)
+                loss += sum(print_losses) / n_totals
 
-            print("[EVAL]Iteration: {}; Loss: {:.4f}".format(iter, loss/len(dev_input_batches)))
-            encoder.train()
-            decoder.train()
+            print("[EVAL]Iteration: {}; Loss: {:.4f}".format(iter, loss))
+
+            set2seq.train()
 
 
 if __name__ == '__main__':
