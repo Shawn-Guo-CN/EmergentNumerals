23ce91952533bf21cd8db974b980d101df4e067e
Shawn Guo <shawnguo.cn@gmail.com>
2019-06-27 14:27:30 +0100

    1. finished the draft of slides 2. updated calculating function for topological similarity

--- analysis/cal_topological_similarity.py
+++ analysis/cal_topological_similarity.py
@@ -6,7 +6,7 @@ from analysis.get_input_message_pairs import output_perfect_in_msg_pairs
 import editdistance
 
 DATA_FILE = './data/all_data.txt'
-MSG_FILE = './data/perfect_in_msg_pairs.txt'
+MSG_FILE = './data/input_msg_pairs.txt'
 
 def load_in_msg_pairs(file_path=MSG_FILE):
     in_file = open(file_path, 'r')
@@ -22,26 +22,36 @@ def load_in_msg_pairs(file_path=MSG_FILE):
     return im_pairs
 
 
-def msg_ham_dis(msg1, msg2):
+def in_ham_dis(in1, in2):
     dis = 0
     for c in range(args.num_words):
         cur_char = chr(65+c)
-        if not msg1.count(cur_char) == msg2.count(cur_char):
+        if not in1.count(cur_char) == in2.count(cur_char):
             dis += 1
     return dis
 
 
-def msg_edit_dis(msg1, msg2):
+def in_edit_dis(in1, in2):
     dis = 0
     for c in range(args.num_words):
         cur_char = chr(65+c)
-        dis += abs(msg1.count(cur_char) - msg2.count(cur_char))
+        dis += abs(in1.count(cur_char) - in2.count(cur_char))
+    return dis
+
+
+def msg_ham_dis(msg1, msg2):
+    dis = 0
+    dis += max(len(msg1), len(msg2)) - min(len(msg1), len(msg2))
+    for i in range(min(len(msg1), len(msg2))):
+        if not msg1[i] == msg2[i]:
+            dis += 1
     return dis
 
 
 def cal_topological_sim(
         msg_file_path=MSG_FILE, 
-        msg_dis_measure='hamming', 
+        in_dis_measure='hamming',
+        msg_dis_measure='edit',
         corr_method='pearson'):
     in_msg_pairs = load_in_msg_pairs(msg_file_path)
 
@@ -51,11 +61,14 @@ def cal_topological_sim(
     len_pairs = len(in_msg_pairs)
     for i, im_pair in enumerate(in_msg_pairs):
         for j in range(i+1, len_pairs):
-            if msg_dis_measure == 'hamming':
-                mean_distances.append(msg_ham_dis(im_pair[0], in_msg_pairs[j][0]))
-            elif msg_dis_measure == 'edit':
-                mean_distances.append(msg_edit_dis(im_pair[0], in_msg_pairs[j][0]))
-            symbol_distances.append(editdistance.eval(im_pair[1], in_msg_pairs[j][1]))
+            if in_dis_measure == 'hamming':
+                mean_distances.append(in_ham_dis(im_pair[0], in_msg_pairs[j][0]))
+            elif in_dis_measure == 'edit':
+                mean_distances.append(in_edit_dis(im_pair[0], in_msg_pairs[j][0]))
+            if msg_dis_measure == 'edit':
+                symbol_distances.append(editdistance.eval(im_pair[1], in_msg_pairs[j][1]))
+            elif msg_dis_measure == 'hamming':
+                symbol_distances.append(msg_ham_dis(im_pair[1], in_msg_pairs[j][1]))
     
     mean_distances = np.asarray(mean_distances)
     symbol_distances = np.asarray(symbol_distances)
@@ -73,14 +86,14 @@ def cal_topological_sim(
 def main():
     # ins, msgs = generate_perfect_in_msg_pairs(DATA_FILE)
     # output_perfect_in_msg_pairs(MSG_FILE, ins, msgs)
-    sim = cal_topological_sim(msg_dis_measure='hamming', corr_method='pearson')
-    print('pearson', 'hamming', sim)
+    sim = cal_topological_sim(in_dis_measure='hamming', msg_dis_measure='edit', corr_method='pearson')
+    print('pearson', 'hamming+edit', sim)
     sim = cal_topological_sim(msg_dis_measure='edit', corr_method='pearson')
-    print('pearson', 'edit', sim)
+    print('pearson', 'edit+edit', sim)
     sim = cal_topological_sim(msg_dis_measure='hamming', corr_method='spearman')
-    print('spearman', 'hamming', sim)
+    print('spearman', 'hamming+edit', sim)
     sim = cal_topological_sim(msg_dis_measure='edit', corr_method='spearman')
-    print('spearman', 'edit', sim)
+    print('spearman', 'edit+edit', sim)
 
 if __name__ == '__main__':
     main()
