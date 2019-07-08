8d222d300cd23d6aae8857c1cf83c8869dbc69e2
Shawn Guo <shawnguo.cn@gmail.com>
2019-07-08 22:16:24 +0800

    updated training procedure plot function

--- analysis/plot_training_process.py
+++ analysis/plot_training_process.py
@@ -12,29 +12,33 @@ def load_training_status(file_path:str) -> tuple:
     return seq_acc, tok_acc, losses, sim
 
 
-def EMA_smooth(l:list, alpha=0.01) -> list:
-    new_l = [l[1]]
+def EMA_smooth(l:list, alpha=0.005) -> list:
+    if len(l) == 0:
+        return l
+    new_l = [l[0]]
     for i in l:
         new_l.append(alpha * i + (1-alpha) * new_l[-1])
     return new_l
 
 
-def plot_training_into_1_figure(chk_points_paths:list) -> None:
+def plot_training_into_1_figure(chk_points_paths:list, label_list:list) -> None:
     # seq_accs, tok_accs, losses, sims = [], [], [], []
     min_len = inf
 
     fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
-    for file_path in chk_points_paths:
+    for idx, file_path in enumerate(chk_points_paths):
         s_acc, t_ac, loss, sim = load_training_status(file_path)
-        label = file_path.split('.')[1].split('/')[-2]
-        if min_len > len(sim):
-            min_len = len(sim)
+        
+        sim = EMA_smooth(sim)
         s_acc = EMA_smooth(s_acc)
         t_ac = EMA_smooth(t_ac)
-        ax0.plot(sim, label=label, linewidth=0.2)
-        ax1.plot(t_ac, label=label, linewidth=1)
-        ax2.plot(loss, label=label, linewidth=1)
-        ax3.plot(s_acc, label=label, linewidth=1)
+        loss = EMA_smooth(loss)
+
+        linewidth = 0.6
+        ax0.plot(sim, label=label_list[idx], linewidth=linewidth)
+        ax1.plot(t_ac, label=label_list[idx], linewidth=linewidth)
+        ax2.plot(loss, label=label_list[idx], linewidth=linewidth)
+        ax3.plot(s_acc, label=label_list[idx], linewidth=linewidth)
 
     def _set_legend_(ax):
         leg = ax.legend()
@@ -52,12 +56,20 @@ def plot_training_into_1_figure(chk_points_paths:list) -> None:
 
 def main():
     chk_point_path_list = [
+        './params/listener/comp/4_60000_hidden128.tar',
+        './params/listener/holistic/4_60000_hidden128.tar',
         './params/listener/comp/0705_12000_4.tar',
-        './params/listener/holistic/0705_12000_4.tar',
-        # './params/0705_4_38700.tar'
+        './params/listener/holistic/0705_12000_4.tar'
     ]
 
-    plot_training_into_1_figure(chk_point_path_list)
+    label_list = [
+        'comp-hidden128',
+        'holistic-hidden128',
+        'comp-hidden256',
+        'holistic-hidden256'
+    ]
+
+    plot_training_into_1_figure(chk_point_path_list, label_list)
 
 
 if __name__ == '__main__':
