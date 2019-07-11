cb5e168eb2623a7f3c50aa9958389ee05c604285
Shawn Guo <shawnguo.cn@gmail.com>
2019-07-11 23:24:24 +0800

    updated training plot function

--- analysis/plot_training_process.py
+++ analysis/plot_training_process.py
@@ -6,13 +6,15 @@ from utils.conf import args
 
 
 def load_training_status(file_path:str) -> tuple:
+    print('loading '+file_path+'...')
     checkpoint = torch.load(file_path, map_location=torch.device('cpu'))
     records = checkpoint['records']
     print(checkpoint['args'])
+    print('done')
     return records
 
 
-def EMA_smooth(l:list, alpha=0.1) -> list:
+def EMA_smooth(l:list, alpha=0.05) -> list:
     if len(l) == 0:
         return l
     new_l = [l[0]]
@@ -27,10 +29,11 @@ def plot_training_into_1_figure(chk_points_paths:list, label_list:list) -> None:
 
     fig, ((ax0, ax1, ax2), (ax3, ax4, ax5)) = plt.subplots(nrows=2, ncols=3)
     for idx, file_path in enumerate(chk_points_paths):
+        t_s_acc, t_t_acc, t_loss, t_sim, e_t_acc, e_s_acc = load_training_status(file_path)
         records = load_training_status(file_path)
         t_s_acc = records['training_seq_acc']
         t_t_acc = records['training_tok_acc']
-        t_loss = records['traing_loss']
+        t_loss = records['training_loss']
         t_sim = records['training_sim']
         e_t_acc = records['eval_tok_acc']
         e_s_acc = records['eval_seq_acc']
@@ -42,7 +45,7 @@ def plot_training_into_1_figure(chk_points_paths:list, label_list:list) -> None:
         e_t_acc = EMA_smooth(e_t_acc)
         e_s_acc = EMA_smooth(e_s_acc)
 
-        linewidth = 0.6
+        linewidth = 1.0
         ax0.plot(e_s_acc, label=label_list[idx], linewidth=linewidth)
         ax1.plot(t_sim, label=label_list[idx], linewidth=linewidth)
         ax2.plot(t_s_acc, label=label_list[idx], linewidth=linewidth)
@@ -50,35 +53,33 @@ def plot_training_into_1_figure(chk_points_paths:list, label_list:list) -> None:
         ax4.plot(t_loss, label=label_list[idx], linewidth=linewidth)
         ax5.plot(t_t_acc, label=label_list[idx], linewidth=linewidth)
 
-    def _set_legend_(ax):
-        leg = ax.legend()
-        leg_lines = leg.get_lines()
-        plt.setp(leg_lines, linewidth=2)
-    
-    for ax in [ax0, ax1, ax2, ax3, ax4, ax5]:
-        _set_legend_(ax)
-    
-    ax0.title.set_text('Eval Sequence Accuracy')
-    ax1.title.set_text('Topological Similarity')
-    ax2.title.set_text('Training Sequence Accuracy')
-    ax3.title.set_text('Eval Token Accuracy')
-    ax4.title.set_text('Training Loss')
-    ax5.title.set_text('Training Token Accuracy')
+        def _set_legend_(ax):
+            leg = ax.legend()
+            leg_lines = leg.get_lines()
+            plt.setp(leg_lines, linewidth=2)
+
+        for ax in [ax0, ax1, ax2, ax3, ax4, ax5]:
+            _set_legend_(ax)
+
+        ax0.title.set_text('Eval Sequence Accuracy')
+        ax1.title.set_text('Topological Similarity')
+        ax2.title.set_text('Training Sequence Accuracy')
+        ax3.title.set_text('Eval Token Accuracy')
+        ax4.title.set_text('Training Loss')
+        ax5.title.set_text('Training Token Accuracy')
     plt.show()
 
 def main():
     chk_point_path_list = [
-        './params/speaker/set2seq_hidden/4/500_h256_seed1234.tar',
-        './params/speaker/set2seq_hidden/4/500_h128_seed1234.tar',
-        './params/speaker/set2seq_hidden/4/500_h64_seed1234.tar',
-        './params/speaker/set2seq_hidden/4/500_h32_seed1234.tar',
+        './params/listener/comp/2_3000_0711.tar',
+        './params/listener/holistic/2_3000.tar',
+        './params/listener/emergent/2_3000.tar',
     ]
 
     label_list = [
-        '256',
-        '128',
-        '64',
-        '32'
+        'compositional',
+        'holistic',
+        'emergent'
     ]
 
     plot_training_into_1_figure(chk_point_path_list, label_list)
