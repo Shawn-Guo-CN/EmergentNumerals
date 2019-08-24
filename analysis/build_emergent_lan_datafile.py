92a3606bacbc70afc535eb0ac19d24ae77978394
Shawn Guo <shawnguo.cn@gmail.com>
2019-08-24 15:04:21 +0100

    1. updated emergent language reproduction to     incorporate img-based agents 2. fixed distractor bug in ImgDateIterator

--- analysis/build_emergent_lan_datafile.py
+++ analysis/build_emergent_lan_datafile.py
@@ -2,9 +2,10 @@ import torch
 
 from models.Set2Seq2Seq import Set2Seq2Seq
 from models.Set2Seq2Choice import Set2Seq2Choice
+from models.Img2Seq2Choice import Img2Seq2Choice
 from utils.conf import args
 from analysis.training_sim_check import reproduce_msg_set
-from preprocesses.DataIterator import FruitSeqDataset, ChooseDataset
+from preprocesses.DataIterator import FruitSeqDataset, ChooseDataset, ImgChooseDataset
 
 
 def msg2str(msg):
@@ -24,30 +25,61 @@ def build_listener_training_file(model, in_set, batch_set, file_path='data/rebui
     out_file.close()
 
 
-if __name__ == '__main__':
+def main(
+    model_name='Img2Seq2Choice',
+    dataset_name='ImgChooseDataset',
+    out_file_path='data/tmp.txt',
+):
     if args.param_file is not None:
         checkpoint = torch.load(args.param_file, map_location=torch.device('cpu'))
     else:
         raise ValueError
 
     print('rebuilding vocabulary and model...')
-    voc = checkpoint['voc']
+    voc = checkpoint['voc'] if model_name == 'Set2Seq2Seq' or model_name == 'Set2Seq2Choice' else None
     train_args = checkpoint['args']
     print(train_args)
-    model = Set2Seq2Seq(voc.num_words, msg_length=train_args.max_msg_len, msg_vocsize=train_args.msg_vocsize, 
-                        hidden_size=train_args.hidden_size, dropout=train_args.dropout_ratio, msg_mode=train_args.msg_mode
-                        ).to(torch.device('cpu'))
-    # model = Set2Seq2Choice(voc.num_words, msg_length=train_args.max_msg_len, msg_vocsize=train_args.msg_vocsize, 
-    #                 hidden_size=train_args.hidden_size, dropout=train_args.dropout_ratio, msg_mode=train_args.msg_mode
-    #                 ).to(torch.device('cpu'))
+
+    if model_name == 'Img2Seq2Choice':
+        model = Img2Seq2Choice(
+                msg_length=train_args.max_msg_len, msg_vocsize=train_args.msg_vocsize,
+                hidden_size=train_args.hidden_size, dropout=train_args.dropout_ratio, msg_mode=train_args.msg_mode
+            ).to(torch.device('cpu'))
+    elif model_name == 'Set2Seq2Seq':
+        model = Set2Seq2Seq(
+                voc.num_words, msg_length=train_args.max_msg_len, msg_vocsize=train_args.msg_vocsize, 
+                hidden_size=train_args.hidden_size, dropout=train_args.dropout_ratio, msg_mode=train_args.msg_mode
+            ).to(torch.device('cpu'))
+    elif model_name == 'Set2Seq2Choice':
+        model = Set2Seq2Choice(
+                voc.num_words, msg_length=train_args.max_msg_len, msg_vocsize=train_args.msg_vocsize, 
+                hidden_size=train_args.hidden_size, dropout=train_args.dropout_ratio, msg_mode=train_args.msg_mode
+            ).to(torch.device('cpu'))
+    else:
+        raise NotImplementedError
+    
     model.load_state_dict(checkpoint['model'])
     model.eval()
     print('done')
     
     print('loading and building batch dataset...')
-    batch_set = FruitSeqDataset(voc, dataset_file_path=args.data_file, batch_size=1, device=torch.device('cpu'))
-    # batch_set = ChooseDataset(voc, dataset_file_path=args.data_file, batch_size=1, device=torch.device('cpu'))
-    in_set = FruitSeqDataset.load_stringset(args.data_file)
+    if dataset_name == 'ImgChooseDataset':
+        batch_set = ImgChooseDataset(dataset_dir_path=args.data_file, batch_size=1, device=torch.device('cpu'))
+        in_set = [batch['correct']['label'][0] for batch in batch_set]
+    elif dataset_name == 'FruitSeqDataset':
+        batch_set = FruitSeqDataset(voc, dataset_file_path=args.data_file, batch_size=1, device=torch.device('cpu'))
+        in_set = FruitSeqDataset.load_stringset(args.data_file)
+    elif dataset_name == 'ChooseDataset':
+        batch_set = ChooseDataset(voc, dataset_file_path=args.data_file, batch_size=1, device=torch.device('cpu'))
+        in_set = FruitSeqDataset.load_stringset(args.data_file)
     print('done')
 
-    build_listener_training_file(model, in_set, batch_set, 'data/4_emergent_gen/all_data.txt')
+    build_listener_training_file(model, in_set, batch_set, out_file_path)
+
+
+if __name__ == '__main__':
+    main(
+        model_name='Img2Seq2Choice',
+        dataset_name='ImgChooseDataset',
+        out_file_path='./data/img_languages/emergent/test.txt',
+    )
